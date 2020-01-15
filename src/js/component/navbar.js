9330d9982cb2c4d3a465a1cb03b4abacf3e1d06a
Evan Otero <Evan.otero19@gmail.com>
2020-01-15 20:38:55 +0000

    Created Header

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,8 +4,21 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 	return (
 		<nav className="container-fluid navbar">
-			<div className="title">
-				<h2>Pokedex Go</h2>
+			<div className="row navrow">
+				<div className="col-md-3 something row">
+					<div className="Lcircle">
+						<div className="Scircle" />
+					</div>
+					<div>
+						<div className="smallcircle scred" />
+						<div className="smallcircle scyellow" />
+						<div className="smallcircle scgreen" />
+					</div>
+				</div>
+				<div className="title col-md-6">
+					<h2>Pokedex Go</h2>
+				</div>
+				<div className="col-md-3" />
 			</div>
 		</nav>
 	);
